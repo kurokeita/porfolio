f701984c42255c0857145c18affecf2128290f9d
kurokeita <kuro.keita94@gmail.com>
2021-06-29 01:28:22 +0700

    init my portfolio

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "Uniform NEXT",
+    subtitle: "EC-CUBE, Yii2, MySQL, Docker",
+    description:
+      "An e-commerce platform built with EC-CUBE PHP framework",
+    image: "./uniformnext.gif",
+    link: "https://www.uniformnext.com/",
+  },
+  {
+    title: "Tenorino Video App",
+    subtitle: "Laravel 7.0, MySQL, Docker, Firebase, AWS Cloud Storage, AWS Cloudfront",
+    description:
+      "A free childcare video app that can easily and safely deliver videos from school and teachers to parents with smartphone",
+    image: "./tenorino.gif",
+    link: "https://www.tenorino.app/",
+  },
+  {
+    title: "Notifun",
+    subtitle: "Laravel 5.8, MySQL, MongoDB, Redis, Cronjob, ReactJS",
+    description:
+      "An internal tool for Funtap employees to send emails, push notifications, Facebook messages and comments to customers",
+    image: "./notifun.gif",
+    link: "https://notifun.com/",
+  },
+  {
+    title: "NineHouse",
+    subtitle: "Laravel 6.0, MySQL, MongoDB, Redis, Cronjob, Docker, ReactJS, Firebase, Google Cloud Storage, Flutter",
+    description:
+      "An application to help house owners, apartment building managerd, landlords... manage their houses, buildings (tenants, invoices, parking, inventory)",
+    image: "./ninehouse.gif",
+    link: "https://landlord.ninehouse.vn/",
+  },
+];
+
+export const skills = [
+  "PHP",
+  "Laravel",
+  "MySQL",
+  "MongoDB",
+  "Redis",
+  "SQL Server",
+  "Git",
+  "Docker",
+  "JavaScript",
+  "ReactJS",
+  "NodeJS",
+  "Windows",
+  "Linux",
+  "MacOS"
+];
